4b6a537588a137d1c5c17ebc1b0f1c8f6cf4cc65
OswalN <cerocinco9@gmail.com>
2024-02-19 23:54:40 -0500

    controller

---
+++ Api/Controllers/EstudianteController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SicoApi.Services.Interface;
+
+namespace SicoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("SicoApiRules")]
+    public class EstudianteController : ControllerBase
+    {
+        private readonly IEstudiante _ContextEstuden;
+
+        public EstudianteController(IEstudiante contextEstuden)
+        {
+            _ContextEstuden = contextEstuden;
+        }
+
+
+        [HttpGet]
+        [Route("ListadoTotal")]
+
+        public async Task<IActionResult> GetListadoEstudiante()
+        {
+            try
+            {
+                var ListEstudent = await _ContextEstuden.ObtenerTodos();
+                return Ok(ListEstudent);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("BuscarPorEstudiante")]
+        public async Task<IActionResult> GetListadoEstudiante(int?id, string? nombre, string? correo)
+        {
+            try
+            {
+
+                var ListEstudent = await _ContextEstuden.Obtener(id, nombre, correo);
+
+
+                return Ok(ListEstudent);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+    }
+}
